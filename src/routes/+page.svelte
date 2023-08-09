30b6987308de82de6145c6a9f3ddbdc8694cf2ce
Ben A <ben.avni5@gmail.com>
2023-08-09 00:20:45 -0500

    don't know how it basically works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import Notes from "$lib/components/Notes.svelte";
-    import {window} from '$lib/stores.js';
+    import { window } from '$lib/stores.js';
+    import { base } from '$app/paths';
 </script>
 
 <div class="flex">
-    <img src="favicon.png" alt="logo" style="width: 30vw; height: 30vw; margin-top: 1rem;"/>
+    <img src={base+"/favicon.png"} alt="logo" style="width: 30vw; height: 30vw; margin-top: 1rem;"/>
     <Notes --width='90%'>
         <h1>Codemaster</h1>
         <p>"Crack the Code" puzzle solver & generator.</p>
